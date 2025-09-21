76dad8745a6243a8dfc2a299d13abf3b0a20aa1b
7anuj <tanujsarkar123@gmail.com>
2025-09-21 16:51:46 +0530

    final i hope so

--- app.py
+++ app.py
@@ -10,6 +10,8 @@ from langchain_community.llms import Ollama
 from langchain.prompts import PromptTemplate
 from langchain.chains import LLMChain
 
+
+
 # --------------------------
 # Sidebar: Mode Selection
 # --------------------------
